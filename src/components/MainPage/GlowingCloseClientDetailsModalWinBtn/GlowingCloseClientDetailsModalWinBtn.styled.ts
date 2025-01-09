db420d30b5155024fc3fe749cccbfa7406b6cb86
Silich Alexander <124524007+sili41995@users.noreply.github.com>
2025-01-09 20:40:58 +0200

    Update GlowingCloseClientDetailsModalWinBtn.styled.ts

--- src/components/MainPage/GlowingCloseClientDetailsModalWinBtn/GlowingCloseClientDetailsModalWinBtn.styled.ts
+++ src/components/MainPage/GlowingCloseClientDetailsModalWinBtn/GlowingCloseClientDetailsModalWinBtn.styled.ts
@@ -8,11 +8,17 @@ export const Button = styled.button`
   width: 54px;
   aspect-ratio: 1 / 1;
   padding: 0;
-  border: 1px solid rgba(255, 255, 255, 0.2);
+  border: 1px solid;
+  border-color: rgba(255, 255, 255, 0.2);
   border-radius: 50%;
   background-color: ${({ theme }) => theme.colors.dark};
+  transition: border-color ${({ theme }) => theme.transitionDurationAndFunc};
 
   & > svg {
     color: ${({ theme }) => theme.colors.white};
+  }
+
+  &:is(:hover, :focus) {
+    border-color: transparent;
   }
 `;
